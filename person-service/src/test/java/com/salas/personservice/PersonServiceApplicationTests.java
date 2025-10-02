488ebabce7577032d047fe8e98a753365f3c13bd
Vladyslav Hasporian <salasengland33@gmail.com>
2025-10-02 21:22:00 +0300

    add person service; configure gradle kts file

---
+++ person-service/src/test/java/com/salas/personservice/PersonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salas.personservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
